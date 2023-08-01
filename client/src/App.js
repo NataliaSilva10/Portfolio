6394f989f87b383bbc43ca4247867213fbd7ecfb
Natalia <nataliamirandaus10@gmail.com>
2023-07-31 22:03:27 -0400

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
 import'./css/style.css';
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
     <div className="App">
-      
+      <ThemeProvider theme={theme}></ThemeProvider>
     </div>
   );
 }
